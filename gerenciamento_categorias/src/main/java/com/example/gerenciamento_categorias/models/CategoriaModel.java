7f507e40baa1b6cf19b37f258627756515d825bb
rafaellafanti <rafaellafantidealmeida@gmail.com>
2026-03-22 18:28:01 -0300

    feat: criação da entidade Categoria

---
+++ gerenciamento_categorias/src/main/java/com/example/gerenciamento_categorias/models/CategoriaModel.java
@@ -0,0 +1,42 @@
+package com.example.gerenciamento_categorias.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class CategoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_categoria")
+    private String nome;
+
+    @Column(name = "descricao_categoria")
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
